a923951072532f21d9f08989e0bff4452e9a4fdf
Kerr Marin Miller <kerr@kerrmarin.com>
2019-10-11 12:29:36 +0100

    Use a better script

--- bin/install-swift.sh
+++ bin/install-swift.sh
@@ -5,8 +5,11 @@ UBUNTU_VERSION_SHORT="1804"
 UBUNTU_VERSION="18.04"
 
 sudo apt-get update
-sudo apt-get install clang libicu-dev libpython2.7
+sudo apt-get install libcurl3 libpython2.7 libpython2.7-dev
 
 wget "https://swift.org/builds/swift-${SWIFT_VERSION}-release/ubuntu${UBUNTU_VERSION_SHORT}/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-ubuntu${UBUNTU_VERSION}.tar.gz"
 tar xzf "swift-${SWIFT_VERSION}-RELEASE-ubuntu${UBUNTU_VERSION}.tar.gz"
-export PATH="swift-${SWIFT_VERSION}-RELEASE-ubuntu${UBUNTU_VERSION}/usr/bin:${PATH}"
+sudo mv "swift-${SWIFT_VERSION}-RELEASE-ubuntu${UBUNTU_VERSION}" /usr/share/swift
+echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
+source  ~/.bashrc
+swift --version
